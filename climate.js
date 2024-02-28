c4011b4cfa106645d88467992fcef22601fc185e
Austin <austinow@gmail.com>
2024-02-27 18:26:45 -0800

    first commit

---
+++ climate.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const ClimateScreen = () => {
+  return (
+    <View style={{ flex:1, 
+                    alignItems: 'center',
+                    justifyContent:'center'}}>
+      <Text>Climate</Text>
+    </View>
+  )
+};
+
+export default ClimateScreen;
