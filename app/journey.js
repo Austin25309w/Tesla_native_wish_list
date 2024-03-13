c48ded7d9ad4aadbba8ddb6109a9a1d64e68fed3
Austin <austinow@gmail.com>
2024-03-13 00:03:14 -0700

    in progress- BottomSheet is able to move up and down

---
+++ app/journey.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const journey = () => {
+  return (
+    <View>
+      <Text>journey</Text>
+    </View>
+  )
+}
+
+export default journey
+
+const styles = StyleSheet.create({})
--- components/BottomSheet.js
+++ components/BottomSheet.js
@@ -1,17 +1,47 @@
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { GestureDetector, Gesture } from 'react-native-gesture-handler'
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
 import React from 'react'
 
-const { height: SCREEN_HEIGHT } = Dimensions.get('window')
+const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 50
 
 const BottomSheet = () => {
+
+    const translateY = useSharedValue(0)
+
+    const context = useSharedValue({ y: 0});
+    const gesture = Gesture.Pan()
+        .onStart(()=> {
+            context.value = { y: translateY.value }
+    }).onUpdate((event) => {
+        translateY.value = event.translationY + context.value.y;
+        translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y);
+    });
+
+    const rBottomSheetStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{translateY: translateY.value }]
+        }
+    })
   return (
-    <View style = {styles.bottomSheetContainer}>
-      <Text style={styles.text}>BottomSheet</Text>
-    </View>
+    <GestureDetector gesture={ gesture}>
+        <Animated.View style = {[styles.bottomSheetContainer, rBottomSheetStyle]}>
+            <View style = {styles.line}>
+            
+            </View>
+            <View style={styles.text}>
+                <Text>BottomSheet</Text>
+            </View>
+            
+        </Animated.View>
+     </GestureDetector>
+    
   )
 }
 
-export default BottomSheet
+
 
 
 const styles = StyleSheet.create({
@@ -19,11 +49,28 @@ const styles = StyleSheet.create({
     bottomSheetContainer: {
          height: 700, 
          width: '100%',
-         backgroundColor: '#161818',
+         backgroundColor: 'white',
          position: 'absolute',
-        //  top: SCREEN_HEIGHT / 1.2 ,
+        //  top: SCREEN_HEIGHT,
+         borderRadius: 25,
     },
     text: {
-        color: 'white'
+        flex: 1,
+        color: 'green',
+        alignItems: 'center',
+        justifyContent: 'center'
+
     },
+    line: {
+        width: 75,
+        height: 4,
+        backgroundColor: 'grey',
+        alignSelf: 'center',
+        marginVertical: 15,
+        borderRadius: 2,
+      },
 })
+
+export default BottomSheet
+
+//#161818
