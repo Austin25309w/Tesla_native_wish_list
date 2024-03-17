18fae5b7c51385e3198a29a9d6db3c57b2dc77e8
Austin <austinow@gmail.com>
2024-03-17 08:26:59 -0700

    finished Journey list and completed feature __ still figure out how to put it on my phone GestureHandlerRootView

--- app/journey.js
+++ app/journey.js
@@ -1,16 +1,44 @@
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, Platform } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, Platform, Keyboard } from 'react-native'
+import React, { useState } from 'react'
 import Task from '../components/Task'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
-const journey = () => {
+export default function Journey() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([])
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask('');
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy)
+  
+  }
   return (
+
+    
     <View style= {styles.container}>
       <View style= {styles.taskWrapper}>
         <Text style= {styles.sectionTitle}>My Journey</Text>
         <View style = {styles.items}> 
-        <Task text= {'Task 1'}/>
-        <Task text={'Task 2'}/>
+        {
+          taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress= {()=> completeTask(index) }>
+                <Task  text= {item} />
+              </TouchableOpacity>
+              
+            )
+            
+          } )
+        }
+        
         
       </View>
       </View>
@@ -19,16 +47,20 @@ const journey = () => {
         behavior= {Platform.OS === "ios" ? "padding" : "height"}
         style= {styles.writeTaskWrapper}
         >
-          <TextInput style={styles.input} placeholder='write a task'/>
-          <TouchableOpacity>
+          <TextInput 
+            style={styles.input} 
+            placeholder='write a task'
+            value = {task}
+            onChangeText={text => setTask(text)}
+          />
+          <TouchableOpacity onPress={() => handleAddTask()}>
             <View style= {styles.addWrapper}>
-              <Text>+</Text>
+              <Text style = {styles.addText}>+</Text>
             </View>
           </TouchableOpacity>
       </KeyboardAvoidingView>
     </View>
-    
-  )
+  );
 }
 
 
@@ -51,18 +83,38 @@ const styles = StyleSheet.create({
     marginTop: 30,
   },
   input:{
-
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
   },
   writeTaskWrapper:{
-
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignContent: 'center'
   },
   addWrapper:{
-
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
   },
+  addText:{
+    fontSize: 25
+  }
 
 })
 
-export default journey
 
 
 
