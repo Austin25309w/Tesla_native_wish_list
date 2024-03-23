64f2ccc532a2cf43b2c4d88ac1c7a03a44979e51
Austin <austinow@gmail.com>
2024-03-22 23:37:27 -0700

    adding mongodb and database

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const { createServer } = require('node:http');
+const { MongoClient } = require('mongodb');
 
+const uri = 'mongodb://localhost:27017';
+
+const client = new MongoClient(uri);
+
+async function connectToMongoDB() {
+    try {
+        // connect the client to the MongoDB server
+        await client.connect();
+        console.log('Connected to MongoDB');
+        // Code for database operations goes here
+        // e.g., insert, update, delete, find operations
+    } catch (error){
+        console.error('Error connecting to MongoDB:', error);
+    } finally {
+        //close the client when done
+        await client.close();
+    }
+}
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -12,3 +31,5 @@ const server = createServer((req,res) => {
 server.listen(port, hostname, ()=> {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+connectToMongoDB();
