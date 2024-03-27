6ee1ed5438dc8f4d6bf53cfa57287391d612e792
Austin <austinow@gmail.com>
2024-03-26 23:54:27 -0700

    started a plan

---
+++ Tamplete/reference.js
@@ -0,0 +1,8 @@
+
+// plan
+//3/26/2024
+// figure out how to start react native and node at the same time when start front end server
+
+
+// reference 
+// https://www.asapdevelopers.com/build-a-react-native-login-app-with-node-js-backend/
