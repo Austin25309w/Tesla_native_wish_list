b8fc7a9c0238f92712b1d0865b89160fe8ea952b
Austin <austinow@gmail.com>
2024-04-08 23:59:33 -0700

    robotaxi

---
+++ app/robotaxi.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function robotaxi() {
+  return (
+    <View>
+      <Text>robotaxi</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
